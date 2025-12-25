d8fa617ee3b03ce6ef63c33baa07a851ee5a128f
mahmoudadelmansour <mssmahmoud4@gmail.com>
2025-12-25 03:30:07 +0200

    Complete The Parser By Line And Note Taker

--- app/FoodicsParser.php
+++ app/FoodicsParser.php
@@ -24,7 +24,32 @@ class FoodicsParser implements WebhookParser
         }
         return $transactions;
     }
+    private function parseLine(string $line): array
+    {
+        $parts = explode('#', $line);
+        // Date,Amount Parsing
+        [$dateString, $amountString] = explode(',', $parts[0], 2);
+        $date = DateParser::parse($dateString);
+        $amount = (float) str_replace(',', '.', $amountString);
+        $reference = $parts[1];
+        $metadata = $this->parseMetadata($parts[2]);
 
+        return [$date, $amount, $reference, $metadata];
+    }
+    private function parseMetadata(string $metadataString): array
+    {
+
+        $pairs = explode('/', $metadataString);
+        $metadata = [];
+
+        // Loop through pairs: key, value, key, value...
+        for ($i = 0; $i < count($pairs); $i += 2) {
+            if (isset($pairs[$i + 1])) {
+                $metadata[$pairs[$i]] = $pairs[$i + 1];
+            }
+        }
+        return $metadata;
+    }
 
 
 }
