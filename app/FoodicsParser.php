c0ba07bbe0eb2c0fe5f077e00b839a000abc8ebe
mahmoudadelmansour <mssmahmoud4@gmail.com>
2025-12-25 03:29:11 +0200

    Create A Strategy Pattern Foodics Parser Class

--- app/FoodicsParser.php
+++ app/FoodicsParser.php
@@ -2,4 +2,29 @@
 
 namespace App;
 
-    class FoodicsParser {}
+use App\Parsing\WebhookParser;
+use App\Utility\DateParser;
+use Illuminate\Support\Carbon;
+
+class FoodicsParser implements WebhookParser
+{
+
+    public function parse(string $payload, string $bankName): array
+    {
+        $lines = explode("\n", $payload);
+        $transactions = [];
+        foreach ($lines as $line) {
+            [$date,$amount,$reference,$metadata] = $this->parseLine($line);
+            $transactions[] = [
+                'date' => $date,
+                'amount' => $amount,
+                'reference' => $reference,
+                'metadata' => $metadata
+            ];
+        }
+        return $transactions;
+    }
+
+
+
+}
